43a422de961a565dcfc3dc0511673ee0ec32c9ed
fawaz-exe <fawaz.exe@gmail.com>
2024-05-01 23:10:08 +0530

    nav not working

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { hamburger } from '../assets/icons/index.js'
+import { headerLogo } from '../assets/images/index.js'
+
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header >
+        <nav className='flex justify-between items-center max-container'>
+           <a href="/">
+            <img src={headerLogo} alt='Logo' width={130} height={29} />
+            </a> 
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+
+            </ul>
+        </nav>
+    </header>
   )
 }
 
-export default Nav
+export default Nav;
