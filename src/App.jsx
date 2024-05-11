c4660d5e1fc0ab7dda7eef1b8a936521f70b95aa
fawaz-exe <fawaz.exe@gmail.com>
2024-05-11 19:41:19 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = ()=>{
     <main className="relative">
       <Nav/>
       <section className="xl:padding-l wide:padding-r padding-b">
-        <Hero/> 
+        <Hero/>
       </section>
       <section className="padding">
         <PopularProducts/>
